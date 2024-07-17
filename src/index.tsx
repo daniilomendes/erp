d145762ab322b6906fc309ec5ef76e5e108f0ce8
Danilo Mendes <danilo.mirandha@gmail.com>
2024-07-17 08:16:13 -0300

    feat: add init redux setup

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,19 @@ import { HelmetProvider } from 'react-helmet-async';
 import { BrowserRouter } from 'react-router-dom';
 
 import 'nprogress/nprogress.css';
+import { Provider } from 'react-redux';
 import App from 'src/App';
 import { SidebarProvider } from 'src/contexts/SidebarContext';
 import * as serviceWorker from 'src/serviceWorker';
+import { store } from './utils/redux/store';
 
 ReactDOM.render(
   <HelmetProvider>
     <SidebarProvider>
       <BrowserRouter>
-        <App />
+        <Provider store={store}>
+          <App />
+        </Provider>
       </BrowserRouter>
     </SidebarProvider>
   </HelmetProvider>,
