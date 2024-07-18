87d32899e21f124f08f14f68e3074d9428b57e7b
Danilo Mendes <danilo.mirandha@gmail.com>
2024-07-18 11:04:59 -0300

    feat: add SelectEmployee component

---
+++ src/components/SelectEmployee/index.tsx
@@ -0,0 +1,45 @@
+import { FormControl, InputLabel, MenuItem, Select } from '@mui/material';
+import { useEffect, useState } from 'react';
+import { Employee } from 'src/models/Employee';
+import { useRequests } from 'src/utils/requests';
+
+type Props = {
+  selectedEmployee: number | '';
+  setSelectedEmployee: (employee_id: number) => void;
+};
+
+const SelectEmployee = ({ selectedEmployee, setSelectedEmployee }: Props) => {
+  const [employeesData, setEmployeesData] = useState<Employee[]>([]);
+
+  const { getEmployees } = useRequests();
+
+  const handleGetEmployees = async () => {
+    const response = await getEmployees();
+
+    if (!response.detail) {
+      setEmployeesData(response.data.employees);
+    }
+  };
+
+  useEffect(() => {
+    handleGetEmployees();
+  }, []);
+
+  return (
+    <FormControl fullWidth>
+      <InputLabel>Selecione um funcionário</InputLabel>
+      <Select
+        value={selectedEmployee}
+        label="Selecione um funcionário"
+        onChange={(e) => setSelectedEmployee(+e.target.value)}
+      >
+        {employeesData.map((item) => (
+          <MenuItem key={item.id} value={item.id}>
+            {item.name} - {item.email}
+          </MenuItem>
+        ))}
+      </Select>
+    </FormControl>
+  );
+};
+export default SelectEmployee;
